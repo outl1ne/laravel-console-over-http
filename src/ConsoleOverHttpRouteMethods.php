ec88f2941c4f417bfd465c95eff8a6f5f6ca780a
Allan Tatter <allan.tatter@gmail.com>
2021-12-02 13:00:07 +0200

    fix duplicate route name

--- src/ConsoleOverHttpRouteMethods.php
+++ src/ConsoleOverHttpRouteMethods.php
@@ -7,8 +7,8 @@ class ConsoleOverHttpRouteMethods
     public function console()
     {
         return function ($endpoint = 'console') {
-            $this->get($endpoint, '\OptimistDigital\LaravelConsoleOverHttp\ConsoleOverHttpController@console')->middleware(Middleware::class)->name('console');
-            $this->post($endpoint, '\OptimistDigital\LaravelConsoleOverHttp\ConsoleOverHttpController@console')->middleware(Middleware::class)->name('console');
+            $this->get($endpoint, '\OptimistDigital\LaravelConsoleOverHttp\ConsoleOverHttpController@console')->middleware(Middleware::class)->name('console-over-http-get');
+            $this->post($endpoint, '\OptimistDigital\LaravelConsoleOverHttp\ConsoleOverHttpController@console')->middleware(Middleware::class)->name('console-over-http-post');
         };
     }
 }
